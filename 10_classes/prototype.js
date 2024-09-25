dfea1395b05ea2af59fdb91aca33371915849d84
Abhay Kushwaha <80638830+justabhay@users.noreply.github.com>
2024-09-25 23:16:00 +0530

    Add files via upload

---
+++ 10_classes/prototype.js
@@ -0,0 +1,67 @@
+// let myName = "abhay     "
+// let mychannel = "chai     "
+
+// console.log(myName.trueLength);
+
+
+let myHeros = ["thor", "spiderman"]
+
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+Object.prototype.abhay = function(){
+    console.log(`abhay is present in all objects`);
+}
+
+Array.prototype.heyAbhay = function(){
+    console.log(`Abhay says hello`);
+}
+
+// heroPower.abhay()
+// myHeros.abhay()
+// myHeros.heyAbhay()
+// heroPower.heyAbhay()
+
+// inheritance
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherUsername = "ChaiAurCode     "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"hitesh".trueLength()
+"iceTea".trueLength()
